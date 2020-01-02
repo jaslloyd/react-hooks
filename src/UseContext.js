b2cb0664719cec5114853a566e227e97ec18e9f9
Jason Lloyd <jason.lloyddev@gmail.com>
2020-01-02 11:46:12 +0000

    Add simple useContext example

---
+++ src/UseContext.js
@@ -0,0 +1,38 @@
+import React, { useContext, createContext } from "react";
+
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
+
+const ThemeContext = createContext(themes.light);
+
+function ContextApp() {
+  return (
+    <ThemeContext.Provider value={themes.dark}>
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
+}
+
+function Toolbar(props) {
+  return <ThemedButton />;
+}
+
+function ThemedButton() {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context!
+    </button>
+  );
+}
+
+export { ContextApp };
